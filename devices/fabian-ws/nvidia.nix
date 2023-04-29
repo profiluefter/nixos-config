886d50b5b0a2977b816e1048f0a14bf2c73210f2
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-04-29 18:23:17 +0200

    Add nvidia config option which should fix suspending issues

--- devices/fabian-ws/nvidia.nix
+++ devices/fabian-ws/nvidia.nix
@@ -1,6 +1,9 @@
-{ config, ... }:
+{ ... }:
 {
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl.enable = true;
   hardware.nvidia.modesetting.enable = true;
+  # Should make graphic glitches after suspending better
+  # but actually makes it worse currently
+  hardware.nvidia.powerManagement.enable = true;
 }
