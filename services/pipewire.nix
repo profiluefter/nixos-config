88f941f648c58b2222b6c7c80c7ab17b7341f2ba
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-09-09 18:53:54 +0200

    Enable bluetooth audio

--- services/pipewire.nix
+++ services/pipewire.nix
@@ -3,6 +3,7 @@
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true;
   security.rtkit.enable = true;
 
   services.pipewire = {
