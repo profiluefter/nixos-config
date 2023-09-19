4051a44ee0c7deb4c845b1b9d2ecd381c75b2ad1
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-09-19 18:17:54 +0200

    Reenable envy drive config

--- devices/envy/hardware-configuration.nix
+++ devices/envy/hardware-configuration.nix
@@ -11,17 +11,17 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-#  profi.systemPurity = {
-#    enable = true;
-#    rollback = true;
-#  };
-#
-#  profi.partitions = {
-#    enable = true;
-#    btrfsDevice = "/dev/disk/by-uuid/4dfdf851-9acb-42d4-b45b-de50bee04755";
-#    subvolPrefix = "nix-os/";
-#    bootDevice = "/dev/disk/by-uuid/EC6A-D41C";
-#  };
+  profi.systemPurity = {
+    enable = true;
+    rollback = true;
+  };
+
+  profi.partitions = {
+    enable = false;
+    btrfsDevice = "/dev/disk/by-partlabel/disk-ssd-btrfs";
+    subvolPrefix = "nix-os/";
+    bootDevice = "/dev/disk/by-partlabel/disk-ssd-esp";
+  };
 
   disko.devices = (import ./drives.nix { ssd-disk = "/dev/nvme0n1"; }).disko.devices;
 
