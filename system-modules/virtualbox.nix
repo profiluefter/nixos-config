9a59011557ca8b11a0817ec84ecca42d944f3c9b
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-10-21 16:20:25 +0200

    Install vagrant

--- system-modules/virtualbox.nix
+++ system-modules/virtualbox.nix
@@ -3,4 +3,5 @@
   virtualisation.virtualbox.host.enable = lib2.hasWorkload config "desktop";
   virtualisation.virtualbox.host.package = pkgs.unstable.virtualbox;
   users.extraGroups.vboxusers.members = lib2.mkIfWorkload config "desktop" [ "fabian" ];
+  environment.systemPackages = lib2.mkIfWorkload config "desktop" [ pkgs.vagrant ];
 }
