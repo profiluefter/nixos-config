b5f07e4644c5613b45114c219df41a3a30085ca7
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-12-02 19:12:00 +0100

    Disable nvidia power management

--- devices/fabian-ws/nvidia.nix
+++ devices/fabian-ws/nvidia.nix
@@ -4,6 +4,7 @@
   hardware.opengl.enable = true;
   hardware.nvidia.modesetting.enable = true;
   # Should make graphic glitches after suspending better
-  # but actually makes it worse currently
-  hardware.nvidia.powerManagement.enable = true;
+  # but actually makes it worse currently, so it's disabled for now
+  # tracking issue: https://github.com/NixOS/nixpkgs/issues/254614
+  hardware.nvidia.powerManagement.enable = false;
 }
