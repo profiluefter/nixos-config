93e965426afb2fe28f5a0b40001aeb0513b0082b
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2024-01-09 22:07:36 +0100

    Fix screen flickering

--- devices/fabian-ws/amdgpu.nix
+++ devices/fabian-ws/amdgpu.nix
@@ -10,4 +10,10 @@
   hardware.opengl.extraPackages = with pkgs; [
     rocmPackages.clr.icd
   ];
+
+  # Fixes screen flickering due to GPU frequency scaling
+  # see https://wiki.archlinux.org/title/AMDGPU#Screen_artifacts_and_frequency_problem
+  services.udev.extraRules = ''
+    KERNEL=="card0", SUBSYSTEM=="drm", DRIVERS=="amdgpu", ATTR{device/power_dpm_force_performance_level}="high"
+  '';
 }
