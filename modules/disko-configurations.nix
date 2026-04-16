aa0d6c557a80cadd607c8ca48b8a3f72708a75e3
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2026-04-16 16:57:14 +0200

    Remove exposed disko configurations for now

--- modules/disko-configurations.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-{
-  flake = {
-    # TODO: check/validate disko configuration
-    diskoConfigurations.envy = import ../devices/envy/drives.nix;
-    diskoConfigurations.framework = import ../devices/framework/drives.nix;
-  };
-}
