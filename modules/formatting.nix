8cbaf6e533499f6fc3f599c5a0e102686a43cf40
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2026-04-16 16:47:34 +0200

    Refactor treefmt

--- modules/formatting.nix
+++ modules/formatting.nix
@@ -3,7 +3,29 @@
   perSystem =
     { pkgs, ... }:
     let
-      treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ../treefmt.nix;
+      treefmtConfig =
+        { ... }:
+        {
+          projectRootFile = "flake.nix";
+          settings.global.excludes = [
+          ];
+
+          programs.nixfmt.enable = true;
+          programs.deadnix.enable = true;
+
+          programs.mdformat.enable = true;
+
+          programs.shellcheck.enable = true;
+          programs.shfmt.enable = true;
+
+          programs.yamlfmt.enable = true;
+          programs.yamlfmt.settings = {
+            formatter = {
+              "retain_line_breaks_single" = true;
+            };
+          };
+        };
+      treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs treefmtConfig;
     in
     {
       checks.formatting = treefmtEval.config.build.check self;
